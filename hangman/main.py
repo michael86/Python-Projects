32a74e9cb5267674572b0699e73790b536022061
michael86 <michael8t6@gmail.com>
2020-02-01 16:11:18 +0000

    Started converting GUI to use canvas instead of grid
    This allows us to draw text with transparency, blending the text in with the background image... Also... Probably need to find a better looking background image.

--- hangman/main.py
+++ hangman/main.py
@@ -1,34 +1,43 @@
-from tkinter import *
+from functions import HmFunctions
+from words import WordGenerator
+import tkinter as tk
 import tkinter.font as font
+from PIL import ImageTk, Image
 
-root = Tk()
-default_font = font.Font(family='comic sans ms')
-root.geometry("600x400")
-root.resizable(width=False, height=False)
+function = HmFunctions()
+word_generator = WordGenerator()
 
-# elements
+new_word = word_generator.generate_new_word()
 
-target_word = Label(
-  root, font=("comic sans ms",  25), text="_ _ _ _ _ _ _ _ _ _ _"
+# Set up the root window.
+root = tk.Tk()  # Initialise root GUI
+root.configure(background='black')
+root.title("Hangman")
+default_font = font.Font(family='comic sans ms')  # Set default font to comic sans
+root.geometry("600x400")  # Set our gui width and height
+root.resizable(width=False, height=False)  # Set resizable to False
+
+# Create our canvas
+canvas = tk.Canvas(root, height=400, width=600)
+canvas.pack()
+
+# Create our image, resembling background.
+photo = ImageTk.PhotoImage(Image.open("background_image.jpg"))  # Import bg image
+background_label = canvas.create_image((0, 0), image=photo, anchor=tk.N+tk.W)
+
+# Display our target word as underscores.
+target_word = canvas.create_text(
+  (300, 40), font=("comic sans ms",  25), text=function.show_length(new_word), fill='white'
   )
 
-guess_counter = Label(root, font=("comic sans ms",  25))
-new_game = Button(root, text="Start New Game")
+guess_counter = tk.Label(root, font=("comic sans ms",  25))
+new_game = tk.Button(root, text="Start New Game")
 new_game['font'] = default_font
-easy = Button(root, text='Easy')
-hard = Button(root, text='Hard')
-user_input = Entry(root)
-padding_left_x = Label(root, padx=10)
-padding_right_x = Label(root, padx=10)
-
-# draw elements
-padding_left_x.grid(row=0, column=0)
-target_word.grid(row=1, column=2)
-new_game.grid(row=2, column=1)
-easy.grid(row=3, column=2)
-easy['font'] = default_font
-hard.grid(row=3, column=3)
-hard['font'] = default_font
+easy = tk.Button(root, text='Easy')
+hard = tk.Button(root, text='Hard')
+user_input = tk.Entry(root)
+padding_left_x = tk.Label(root, padx=10)
+padding_right_x = tk.Label(root, padx=10)
 
 # start our program
 root.mainloop()
